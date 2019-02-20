14597638596125112d2590612c3169bcf852845e
ao cai <aocai1993@gmail.com>
2019-02-20 10:45:18 -0800

    server

--- face_landmarks_pi.py
+++ face_landmarks_pi.py
@@ -3,11 +3,18 @@ import cv2
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import argparse
+import http.client
+
+#Set up http connection
+
+
+
+
 
 # Set up camera constants
 IM_WIDTH = 1280
 IM_HEIGHT = 720
-
+   
 # Select camera type (if user enters --usbcam when calling this script,
 # a USB webcam will be used)
 camera_type = 'picamera'
@@ -30,7 +37,7 @@ if __name__ == "__main__":
         # Initialize Picamera and grab reference to the raw capture
         camera = PiCamera()
         camera.resolution = (IM_WIDTH, IM_HEIGHT)
-        camera.framerate = 30
+        camera.framerate = 60
         rawCapture = PiRGBArray(camera, size=(IM_WIDTH, IM_HEIGHT))
         rawCapture.truncate(0)
         for frame1 in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
@@ -82,7 +89,7 @@ if __name__ == "__main__":
                     x = shape.part(i).x
                     y = shape.part(i).y
                     cv2.circle(frame, (x, y), 3, (0, 150, 255))
-                    # cv2.putText(frame, str(i), (x+2, y+4), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (100, 200, 200), 1)
+                    cv2.putText(frame, str(i), (x+2, y+4), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (100, 200, 200), 1)
 
             cv2.putText(frame, "FPS: {0:.2f}".format(frame_rate_calc), (30, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2, cv2.LINE_AA)
 
